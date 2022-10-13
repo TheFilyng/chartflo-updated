75bb0291b398a6cbb9bb747dba527da6d6d237f6
Agustín Barruti <agustin@chubbyapps.com>
2022-10-13 13:19:44 +0200

    Reimplement resample to resample a list of numbers instead of one

--- lib/src/timeseries/timeframe.dart
+++ lib/src/timeseries/timeframe.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:ml_linalg/linalg.dart';
+import 'dart:collection';
 import '../types.dart';
 
 // ignore: avoid_classes_with_only_static_members
@@ -24,25 +25,32 @@ class TimeFrame {
     return data;
   }
 
-  static Map<DateTime, num> resample(
-      {@required Map<DateTime, num> dataset,
+  static Map<DateTime, List<num>> resample(
+      {@required Map<DateTime, List<num>> dataset,
       @required Duration timePeriod,
       ResampleMethod resampleMethod = ResampleMethod.mean}) {
-    final ds = <DateTime, num>{};
+    final ds = <DateTime, List<num>>{};
     _splitFromDuration(dataset, timePeriod).forEach((subSerie) {
       final startDate = subSerie.first.keys.toList().first;
       final endDate = subSerie.last.keys.toList().first;
       // resample numbers
       final row = subSerie
-          .map<double>((row) => row[row.keys.toList()[0]].toDouble())
+          .map<List<double>>((row) =>
+              row[row.keys.toList()[0]].map((e) => e.toDouble()).toList())
           .toList();
-      num value;
+      List<num> value;
       switch (resampleMethod) {
         case ResampleMethod.mean:
-          value = Vector.fromList(row).mean();
+          for (var vals in row) {
+            var average = Vector.fromList(vals).mean();
+            value.add(average);
+          }
           break;
         case ResampleMethod.sum:
-          value = Vector.fromList(row).sum();
+          for (var vals in row) {
+            var average = Vector.fromList(vals).sum();
+            value.add(average);
+          }
           break;
       }
       final date = _midDate(startDate, endDate);
@@ -57,21 +65,21 @@ DateTime _midDate(DateTime startDate, DateTime endDate) {
   return endDate.subtract(offset);
 }
 
-List<List<Map<DateTime, num>>> _splitFromDuration(
-    Map<DateTime, num> dataset, Duration timePeriod) {
-  final res = <List<Map<DateTime, num>>>[];
+List<List<Map<DateTime, List<num>>>> _splitFromDuration(
+    Map<DateTime, List<num>> dataset, Duration timePeriod) {
+  final res = <List<Map<DateTime, List<num>>>>[];
   final startDate = dataset.keys.toList()[0];
   var nextDate = startDate.add(timePeriod);
-  var subSerie = <Map<DateTime, num>>[];
+  var subSerie = <Map<DateTime, List<num>>>[];
   dataset.forEach((date, value) {
-    final record = <DateTime, num>{date: value};
+    final record = <DateTime, List<num>>{date: value};
     switch (date.isBefore(nextDate)) {
       case true:
         subSerie.add(record);
         break;
       case false:
         res.add(subSerie);
-        subSerie = <Map<DateTime, num>>[record];
+        subSerie = <Map<DateTime, List<num>>>[record];
         nextDate = date.add(timePeriod);
     }
   });
