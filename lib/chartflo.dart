e790376b1b8e17a5386a0e0a6749b9b997deece0
Agustín Barruti <agustin@chubbyapps.com>
2022-10-13 11:54:20 +0200

    Removed chart functionality
    This is because this will be aimed to be a timeframe manipulation package.

--- lib/chartflo.dart
+++ lib/chartflo.dart
@@ -3,7 +3,5 @@ library chartflo;
 export 'src/models/models.dart';
 export 'src/radar.dart';
 export 'src/sparkline.dart';
-export 'src/timeseries/real_time.dart';
 export 'src/timeseries/timeframe.dart';
-export 'src/timeseries/timeseries.dart';
 export 'src/types.dart';
